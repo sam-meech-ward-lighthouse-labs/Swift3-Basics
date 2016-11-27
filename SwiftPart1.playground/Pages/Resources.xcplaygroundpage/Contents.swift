14e95fedac1aa08846f843950bf758dc2e61418e
Sam Meech-Ward <sam@meech-ward.com>
2016-11-27 12:49:12 -0800

    Add a link to swift design patterns

--- SwiftPart1.playground/Pages/Resources.xcplaygroundpage/Contents.swift
+++ SwiftPart1.playground/Pages/Resources.xcplaygroundpage/Contents.swift
@@ -7,6 +7,8 @@
  * [Swift 3 Cheat Sheet and Quick Reference](https://www.raywenderlich.com/73967/swift-cheat-sheet-and-quick-reference)
  * [Swift 3 Style Guide](https://github.com/sam-meech-ward-lighthouse-labs/Swift3-Basics)
  * [Swift.org](https://swift.org/)
+ * [Swift 3 Interoperability](https://github.com/sam-meech-ward-lighthouse-labs/Swift3-Interoperability)
+ * [Swift Design Patterns](https://github.com/ochococo/Design-Patterns-In-Swift)
  */
 
 //: [Next](@next)
